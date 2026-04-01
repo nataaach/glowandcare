f3e626e88465e523e2136583ffbce5f3520c3999
nataaach <143094998+nataaach@users.noreply.github.com>
2026-04-01 16:38:18 +0300

    .

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('cosmetics_shop_db', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false
+});
+
+module.exports = sequelize;
